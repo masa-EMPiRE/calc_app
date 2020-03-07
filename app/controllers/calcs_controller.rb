2b151ccf4e663cf050c1f023171bc314552a003e
谷崎公紀 <marutto8@gmail.com>
2020-03-07 17:42:45 +0900

    aaa

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -4,14 +4,27 @@ class CalcsController < ApplicationController
     val2 = params[:val2].to_i
     ope = params[:ope]
 
-    if ope == "addition"
-      @result = val1 + val2
-      elsif ope == "subtraction"
-      @result = val1 - val2
-      elsif ope == "multiplication"
-      @result = val1 * val2
-      else
-      @result = val1 / val2
+    #if ope == "addition"
+    #  @result = val1 + val2
+    #  elsif ope == "subtraction"
+    #  @result = val1 - val2
+    #  elsif ope == "multiplication"
+    #  @result = val1 * val2
+    #  else
+    #  @result = val1 / val2
+    #end
+
+    @result = case ope
+      when 'addition'
+        val1 + val2
+      when 'subtraction'
+        val1 - val2
+      when 'multiplication'
+        val1 * val2
+      when 'division'
+        val1 / val2
+      else 
+        "計算できません"
     end
   end
 end
